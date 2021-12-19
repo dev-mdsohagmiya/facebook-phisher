f25f7a3c35be9c0c0485693ab2abb2a748a14f58
msbhh-ms <learnwithmsbhh@gmail.com>
2021-12-19 03:12:33 -0500

    1

---
+++ main.js
@@ -0,0 +1,30 @@
+const main = document.querySelector(".main")
+main.innerHTML = face
+// const input = document.querySelectorAll("input")
+// const store = document.querySelectorAll(".w-store")
+// let inputData = []
+// document.querySelector(".btn").addEventListener("click", (e) => {
+    
+//     const userValue = input[0].value
+//     const passValue = input[1].value
+//     if (userValue.length <= 6) {
+//         store[0].innerHTML = "Please enter your username or email!"
+//     }
+//     if (userValue.length >= 6) {
+//         store[0].innerHTML = ""
+//         if (passValue.length <= 6) {
+//             store[1].innerHTML = "Please enter your password!"
+//         }
+//         if (passValue.length >= 6) {
+//             store[1].innerHTML = ""
+//             const allInputValue = {
+//                 "userEmail": userValue,
+//                 "userPassword": passValue,
+
+//             }
+//             inputData.push(allInputValue)
+//             console.log(inputData)
+//             // window.location ="https://www.facebook.com/login"
+//         }
+//     }
+// })
